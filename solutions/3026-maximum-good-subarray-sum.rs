fb584fa99ef0295544f5783d1a9579cd3095ed07
l31 <l31@puffer.fish>
2024-07-17 17:20:25 +0200

    fix: optimize maximum subarray sum calculation

--- solutions/3026-maximum-good-subarray-sum.rs
+++ solutions/3026-maximum-good-subarray-sum.rs
@@ -1,16 +1,18 @@
 pub fn maximum_subarray_sum(nums: Vec<i32>, k: i32) -> i64 {
-    let mut max: i32 = 0;
+    let nums_len: usize = nums.len();
+    let mut max_sum: i64 = i64::MIN;
 
-    for i in 0..nums.len() {
-        for j in i..nums.len() {
-            if (nums[i] - nums[j]) == k {
-                let a: i32 = nums[i..j].iter().sum();
-                if a > max {
-                    max = a;
-                }
+    for i in 0..nums_len {
+        let mut subarray_sum: i64 = 0;
+
+        for j in i..nums_len {
+            subarray_sum += nums[j] as i64;
+
+            if ((nums[j] - nums[i]).abs() == k) && (subarray_sum > max_sum) {
+                max_sum = subarray_sum;
             }
         }
     }
 
-    i64::from(max)
+    max_sum
 }
